25bd5297787f727822923e6cd36da550eb4dff9d
Tarun Boricha <tarunboricha2092001@gmail.com>
2023-12-04 19:52:48 +0530

    Hii

--- src/components/Note.js
+++ src/components/Note.js
@@ -3,8 +3,7 @@ import React from "react";
 function Note() {
     return (
         <div class="note">
-        <h1>This is title
-        </h1>
+        <h1>This is title</h1>
         <p>Hello!! how are you? This is content.</p>
         <button>Update</button>
         <button>Delete</button>
