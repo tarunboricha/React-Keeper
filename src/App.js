ecc1c3a6f4264f1f558467547c322ee17d940e2e
VaishaliC10 <chavanvaishu9080@gmail.com>
2023-12-06 17:03:07 +0530

    vaisali commit

--- src/App.js
+++ src/App.js
@@ -10,19 +10,6 @@ function App() {
       <Header></Header>
       <CreateArea></CreateArea>
       <div className="row align-items-start" style={{width:'98%', margin:'0 1%', minHeight:'23vh'}}>
-      <Note t="Hello" c="Hello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?Hello how are you?Hello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?elello how are you?ello how are you?lo how are you?ello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?elello how are you?ello how are you?lo how are you?ello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?elello how are you?ello how are you?lo how are you?ello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?elello how are you?ello how are you?lo how are you?ello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?elello how are you?ello how are you?lo how are you?ello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?"></Note>
-        <Note t="Hello" c="Hello how are you?elello how are you?ello how are you?lo how are you?ello how are you?"></Note>
       </div>
       <Footer></Footer>
     </div>
