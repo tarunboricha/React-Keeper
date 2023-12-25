701d8d1adbcbf2dd9535e7ab67a81a58ec20612e
Tarun Boricha <tarunboricha2092001@gmail.com>
2023-12-25 12:23:16 +0530

    server url change

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -56,7 +56,7 @@ function Signin(props) {
             return;
         }
         setisSpinner(true);
-        const url = `https://e209-103-250-162-221.ngrok-free.app/users/${signindetail.email}/${signindetail.password}`;
+        const url = `https://0d14-103-250-162-221.ngrok-free.app/users/${signindetail.email}/${signindetail.password}`;
         axios.get(url, {headers})
             .then((response) => {
                 setisSpinner(false);
